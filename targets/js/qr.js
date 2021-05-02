920ab8b4a87935fa9f9712c96a24abf91a632873
Ari Stehney <aristehney@gmail.com>
2021-05-02 11:41:07 -0400

    Update Database fetch for QR Mode

--- targets/js/qr.js
+++ targets/js/qr.js
@@ -1,6 +1,16 @@
 const qrParams = new URLSearchParams(window.location.search);
-var manuf = encodeURIComponent(qrParams.get("manf"));
-var prods = encodeURIComponent(qrParams.get("prod"));
 
-document.getElementById("prod-preview").src = `preview/index.html?man=${manuf}&prod=${prods}`;
-document.getElementById("drone-welcome").innerHTML = `Congratulations on puchasing your new ${manuf} drone!`;
+var itemid = encodeURIComponent(qrParams.get("item"));
+
+fetch('/targets/tr.json')
+  .then(response => response.json())
+  .then(data => loadArray(data))
+
+function loadArray(dataarray) {
+    dataarray.forEach(function (item, index) {
+        if (itemid == item[4]) {
+            document.getElementById("prod-preview").src = `preview/index.html?man=${item[1]}&prod=${item[0]}`;
+            document.getElementById("drone-welcome").innerHTML = `Congratulations on puchasing your new ${item[1]} drone!`;
+        }
+    });
+}
